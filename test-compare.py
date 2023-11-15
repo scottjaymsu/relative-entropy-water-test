3b3ce167bf0b9579c6c0030c02292b03bc2a1272
Jay Scott <scottjay@msu.edu>
2023-11-14 23:23:47 -0500

    potential test cases pre-approval

--- test-compare.py
+++ test-compare.py
@@ -1,10 +1,17 @@
 # https://stackoverflow.com/questions/54408060/how-to-compare-cpp-and-python-outputs
 # https://docs.python.org/3/library/unittest.html
+
+# test cases:
+# 1. compare positions
+# 2. compare forces - assertEqual(f_x), assertEqual(f_y), assertEqual(f_z)
+# 3. compare relative entropies
+
+
 # import unittest
 
 # class test_internal_external_methods(unittest.TestCase):
 	# def test_method(self):
-		# self.assertTrue(a,b)
+		# self.assertTrue(a,b) / self.assertEqual(a,b)
 
 # if __name__ == '__main__':
 	# unittest.main()
