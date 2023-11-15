76e393bb5648b56b00c7fc59670f5386f5be4712
Jay Scott <scottjay@msu.edu>
2023-11-14 22:43:42 -0500

    unit test python script

---
+++ test-compare.py
@@ -0,0 +1,10 @@
+# https://stackoverflow.com/questions/54408060/how-to-compare-cpp-and-python-outputs
+# https://docs.python.org/3/library/unittest.html
+# import unittest
+
+# class test_internal_external_methods(unittest.TestCase):
+	# def test_method(self):
+		# self.assertTrue(a,b)
+
+# if __name__ == '__main__':
+	# unittest.main()
